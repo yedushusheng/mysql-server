cb1b61855927f64ec74fc4f3c1875970148b5092
yedushusheng <220143530@seu.edu.cn>
2022-04-03 23:16:07 +0800

    make_join_select

--- sql/sql_optimizer.cc
+++ sql/sql_optimizer.cc
@@ -6064,17 +6064,45 @@ static void trace_table_dependencies(Opt_trace_context *trace,
 
   @returns false on success, true on error
 */
-
+/** NOTE:把列上的非NULL语义加入到条件中,有利于条件判断(语义优化的技术使用)
+ * 为条件子句增加非空条件函数,目的是应用语义优化的技术,对SQL查询语句进一步优化.
+ * 该函数为等式条件子句增加非空条件,但不是所有的等式条件子句都可以增加非空条件.
+ * 非空条件是DDL语句为表增加的属性信息,包括因主键索引创建暗含的列值非空、列定义为非空约束等情况.
+ * 
+ * 例如,对于以下的两表连接,SQL语句如下:
+ * select * from t1,t2 where t2.key=t1.field;
+ * 上条SQL语句的查询优化过程中连接的对象(两个表)可以表达如下:
+ * any-access(t1),ref(t2.key=t1.field)
+ * 上述表达式表达的含义如下:
+ * 访问t1表(any_access)，引用(ref)了t2.key=t1.field这个条件.
+ * 但因为t2表有索引列key,所以可以增加判断条件以便为整体的查询计划做优化.增加条件后的SQL查询的含义可如下:
+ * select * from t1,t2 where t2.key=t1.field and t1.field is not null;
+ * t2.key=t1.field中t2.key列是一个存在主键索引的列,故key不可为NULL,所以根据等式,
+ * 能够判断t1.field也不能为NULL,所以可以为SQL查询增加条件t1.field IS NOT NULL.
+ * MYSQL因为能够使用非空约束支持语义优化,所以MySQL有多处代码在收集列的非空信息.对于列约束为非NULL的情况,MySQL的代码处理方式总结如下:
+ * 1.update_ref_and_keys函数:收集了空值拒绝(null-rejection)的情况(在Key_field::null_rejection).
+ * 有利于进行外连接消除的优化(即明确了哪些列是非空的).
+ * 2.add_key_part函数:保存收集到的空值拒绝信息到Key_use.
+ * 3.create_ref_for_key函数:复制空值拒绝信息到TABLE_REF,这种方式有利于表数据的快速访问.
+ * 4.add_not_null_conds函数:增加x IS NOT NULL到连接树(JOIN_TAB)的成员上的join_tab->m_condition.
+ * 这样有利于条件的判断,这是利用了语义优化技术对SQL进行优化.
+ * 
+ * 由此可知,add_not_null_conds函数被make_join_select函数调用,利用非NULL信息优化表达式
+ * 调用关系:
+ * JOIN::optimzie -> make_join_select -> add_not_null_conds -> and_with_condition
+*/
 static bool add_not_null_conds(JOIN *join) {
   DBUG_TRACE;
   ASSERT_BEST_REF_IN_JOIN_ORDER(join);
   for (uint i = join->const_tables; i < join->tables; i++) {
+  //NOTE:遍历连接树中除常量表外的每个表
     JOIN_TAB *const tab = join->best_ref[i];
     if ((tab->type() != JT_REF && tab->type() != JT_EQ_REF &&
          tab->type() != JT_REF_OR_NULL) ||
-        tab->table()->is_nullable()) {
+        tab->table()->is_nullable()) {//NOTE:不是所有的等式条件子句都可以增加非空条件
       continue;
     }
+    //NOTE:遍历表上的每个key
     for (uint keypart = 0; keypart < tab->ref().key_parts; keypart++) {
       if ((tab->ref().null_rejecting & ((key_part_map)1 << keypart)) == 0) {
         continue;
@@ -6104,6 +6132,7 @@ static bool add_not_null_conds(JOIN *join) {
                    print_where(join->thd, notnull, referred_tab->table()->alias,
                                QT_ORDINARY););
       referred_tab->and_with_condition(notnull);
+      //NOTE:为等式条件子句增加非空条件
     }
   }
   return false;
@@ -9183,7 +9212,7 @@ void JOIN::finalize_derived_keys() {
     make_cond_for_info_schema() uses an algorithm similar to
     make_cond_for_table().
 */
-
+//NOTE:把条件和表对应,以便于下推
 Item *make_cond_for_table(THD *thd, Item *cond, table_map tables,
                           table_map used_table, bool exclude_expensive_cond) {
   /*
@@ -9273,6 +9302,10 @@ Item *make_cond_for_table(THD *thd, Item *cond, table_map tables,
 /** NOTE:对条件尽量求值,并下推约束条件到表中
  * make_join_select函数用于分解连接条件(WHERE/ON子句中的条件),下推选择等条件到表中.
  * 需要注意的是,make_join_select函数通过调用add_not_null_conds函数为列对象增加非空条件,进而利用语义优化技术进行优化.
+ * 调用关系:
+ * JOIN::optimize -> make_join_select -> add_not_null_conds
+ *                                    -> make_cond_for_table
+ *                                    -> and_conditions
 */
 static bool make_join_select(JOIN *join, Item *cond) {
   DBUG_ASSERT(cond == nullptr || cond->is_bool_func());
@@ -9282,12 +9315,17 @@ static bool make_join_select(JOIN *join, Item *cond) {
   ASSERT_BEST_REF_IN_JOIN_ORDER(join);
 
   // Add IS NOT NULL conditions to table conditions:
+  //NOTE:优化点:重要操作.把列上的非NULL语义加入到条件中,有利于条件判断(语义优化的技术使用)
   if (add_not_null_conds(join)) return true;
 
   /*
     Extract constant conditions that are part of the WHERE clause.
     Constant parts of join conditions from outer joins are attached to
     the appropriate table condition in JOIN::attach_join_conditions().
+  */
+  /** NOTE:步骤一:抽取表达式中的常量.调用的函数包括:make_cond_for_table/and_conditions/Item_func_trig_cond
+   * 1)WHERE子句中的常量在抽取的过程中即进行计算,如果有条件不满足,得到空结果集.
+   * 2)ON子句中的常量在抽取的过程中需要封装到Item_func_trig_cond
   */
   if (cond) /* Because of QUICK_GROUP_MIN_MAX_SELECT */
   {         /* there may be a select without a cond. */
@@ -9304,11 +9342,13 @@ static bool make_join_select(JOIN *join, Item *cond) {
     and if it is not satisfied then the join has empty result
   */
   Item *const_cond = nullptr;
+  //NOTE:把条件和表对应,以便于下推
   if (cond)
     const_cond = make_cond_for_table(thd, cond, join->const_table_map,
                                      table_map(0), true);
 
   // Add conditions added by add_not_null_conds()
+  //NOTE:步骤二:遍历每个表,对于可能的条件进行下推
   for (uint i = 0; i < join->const_tables; i++) {
     if (and_conditions(&const_cond, join->best_ref[i]->condition()))
       return true;
